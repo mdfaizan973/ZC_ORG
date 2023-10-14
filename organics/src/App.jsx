5d534689a64cdefc999a09b80966ec6ba19b06ed
mdfaizan973 <faizan.md9735@gmail.com>
2023-10-14 17:51:24 +0530

    added some organic data

--- organics/src/App.jsx
+++ organics/src/App.jsx
@@ -1,14 +1,72 @@
 // import { useState } from "react";
 
 import "./App.css";
+import Navbar from "./Components/Navbar";
 
 function App() {
   return (
     <>
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <Navbar />
     </>
   );
 }
 
 export default App;
-// fruits, vegetables, grains, dairy products and meat
+// !fruits, vegetables, grains, dairy products and meat
+// ?category=vegetables
+
+// !fruits
+// Apples
+// Bananas
+// Strawberries
+// Blueberries
+// Oranges
+// Grapes
+// Kiwi
+// Peaches
+// Pears
+// Plums
+// Cherries
+// Mangoes
+
+// !vegetables
+// Spinach
+// Carrots
+// Broccoli
+// Tomatoes
+// Bell Peppers
+// Kale
+// Cucumbers
+// Zucchini
+// Green Beans
+// Eggplant
+// Radishes
+// Swiss Chard
+
+// !grains
+// Wheat Bread
+// Rice Cakes
+// Quinoa Salad
+// Oatmeal
+// Barley Soup
+// Spaghetti
+// Corn Tortillas
+// Buckwheat Pancakes
+// Millet Porridge
+// Rye Crackers
+// Couscous Salad
+// Farro Risotto
+
+// !dairy
+// Milk
+// Butter
+// Cheese
+// Yogurt
+// Sour Cream
+// Cottage Cheese
+// Cream Cheese
+// Whipping Cream
+// Ice Cream
+// Evaporated Milk
+// Condensed Milk
+// Ghee
--- organics/src/Components/Navbar.jsx
+++ organics/src/Components/Navbar.jsx
@@ -1,5 +1,9 @@
 // import React from 'react'
 
 export default function Navbar() {
-  return <div>Navbar</div>;
+  return (
+    <div>
+      <h1 className="text-3xl font-bold underline">Navbar</h1>
+    </div>
+  );
 }
