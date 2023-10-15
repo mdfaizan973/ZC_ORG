45c48116fd1e3c95277c0bd5cac332798a1d5c38
mdfaizan973 <faizan.md9735@gmail.com>
2023-10-15 14:32:28 +0530

    able to make good looking ui With fetching products

--- organics/src/Pages/Cards/OrgPro.jsx
+++ organics/src/Pages/Cards/OrgPro.jsx
@@ -1,50 +1,61 @@
 // import React from "react";
-
-export default function OrgPro() {
+import PropTypes from "prop-types";
+export default function OrgPro(props) {
+  const { title } = props;
+  // image , price ,title
   return (
     <div>
-      <div className="w-fit mx-auto grid grid-cols-1 lg:grid-cols-4 md:grid-cols-3 sm:grid-cols-2 justify-items-center justify-center gap-y-20 gap-x-14 mt-10 mb-5">
-        <div className="bg-white w-72 shadow-md rounded-xl duration-500 hover:scale-105 hover:shadow-xl">
-          <a href="#">
-            <img
-              src="https://images.unsplash.com/photo-1649261191606-cb2496e97eee?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwcm9maWxlLXBhZ2V8NDR8fHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=500&q=60"
-              alt=""
-              className="h-60 w-72 object-cover rounded-t-xl"
-            />
-            <div className="px-4 py-3 w-72">
-              <span className="text-gray-400 mr-3 uppercase text-xs">
-                Brand
-              </span>
-              <span className="text-red-400 mr-3 text-xs">Rate</span>
-              <p className="text-lg font-bold text-black truncate block capitalize">
-                Name
+      {/* <div className="w-fit mx-auto grid grid-cols-1 lg:grid-cols-4 md:grid-cols-3 sm:grid-cols-2 justify-items-center justify-center gap-y-20 gap-x-14 mt-10 mb-5"> */}
+      <div className="bg-white w-72 shadow-md rounded-xl duration-500 hover:scale-105 hover:shadow-xl">
+        <a href="#">
+          <img
+            // src={image}
+            src="https://i.pinimg.com/564x/6d/1c/fa/6d1cfa4e1a5e58d31cc935fad125e046.jpg"
+            alt=""
+            className="h-60 w-72 object-cover rounded-t-xl"
+          />
+          <div className="px-4 py-3 w-72">
+            {/* <span className="text-red-400 font-bold mr-3 uppercase text-xs">
+              View
+            </span> */}
+            <div className="center relative inline-block select-none whitespace-nowrap rounded-lg bg-green-500 py-1 px-2 align-baseline font-sans text-xs font-bold uppercase leading-none text-white">
+              <div className="mt-px">View</div>
+            </div>
+            <span className="text-red-400 mr-3 text-xs">{title}</span>
+            <p className="text-lg font-bold text-black truncate block capitalize">
+              Title
+            </p>
+            <div className="flex items-center">
+              <p className="text-lg font-semibold text-black cursor-auto my-3">
+                50 ₹
               </p>
-              <div className="flex items-center">
-                <p className="text-lg font-semibold text-black cursor-auto my-3">
-                  100 ₹
-                </p>
-                <del> </del>
-                <div className="ml-auto">
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    width="20"
-                    height="20"
-                    fill="currentColor"
-                    className="bi bi-bag-plus"
-                    viewBox="0 0 16 16"
-                  ></svg>
+              <del> </del>
+              <div className="ml-auto">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="20"
+                  height="20"
+                  fill="currentColor"
+                  className="bi bi-bag-plus"
+                  viewBox="0 0 16 16"
+                ></svg>
 
-                  <div className="ml-auto">
-                    <button className="bg-green-500 hover:bg-green-600 text-white font-bold py-2 px-4 rounded">
-                      Add to Cart
-                    </button>
-                  </div>
+                <div className="ml-auto">
+                  <button className="bg-green-500 hover:bg-green-600 text-white font-bold py-2 px-4 rounded">
+                    Add to Cart
+                  </button>
                 </div>
               </div>
             </div>
-          </a>
-        </div>
+          </div>
+        </a>
       </div>
+      {/* </div> */}
     </div>
   );
 }
+OrgPro.propTypes = {
+  title: PropTypes.string.isRequired,
+  image: PropTypes.string.isRequired,
+  price: PropTypes.number.isRequired,
+};
--- organics/src/Pages/OrganicPro.jsx
+++ organics/src/Pages/OrganicPro.jsx
@@ -1,49 +1,43 @@
-// import React from 'react'
+import { useState, useEffect } from "react";
 
+import OrgPro from "./Cards/OrgPro";
+import axios from "axios";
 export default function OrganicPro() {
+  const [orgData, setOrgData] = useState([]);
+
+  const getData = () => {
+    axios
+      .get("http://localhost:3030/orgproducts?_limit=9&_page=5")
+      .then((res) => {
+        console.log(res.data);
+        setOrgData(res.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+
+  useEffect(() => {
+    getData();
+  }, []);
   return (
     <div>
-      <div className="w-fit mx-auto grid grid-cols-1 lg:grid-cols-4 md:grid-cols-3 sm:grid-cols-2 justify-items-center justify-center gap-y-20 gap-x-14 mt-10 mb-5">
-        <div className="bg-white w-72 shadow-md rounded-xl duration-500 hover:scale-105 hover:shadow-xl">
-          <a href="#">
-            <img
-              src="https://www.organictattva.com/wp-content/uploads/2019/08/Amaranth-cutlet.jpg"
-              alt=""
-              className="h-60 w-72 object-cover rounded-t-xl"
-            />
-            <div className="px-4 py-3 w-72">
-              <span className="text-gray-400 mr-3 uppercase text-xs">
-                Brand
-              </span>
-              <span className="text-red-400 mr-3 text-xs">Rate</span>
-              <p className="text-lg font-bold text-black truncate block capitalize">
-                Name
-              </p>
-              <div className="flex items-center">
-                <p className="text-lg font-semibold text-black cursor-auto my-3">
-                  100 ₹
-                </p>
-                <del> </del>
-                <div className="ml-auto">
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    width="20"
-                    height="20"
-                    fill="currentColor"
-                    className="bi bi-bag-plus"
-                    viewBox="0 0 16 16"
-                  ></svg>
-
-                  <div className="ml-auto">
-                    <button className="bg-green-500 hover:bg-green-600 text-white font-bold py-2 px-4 rounded">
-                      Add to Cart
-                    </button>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </a>
-        </div>
+      <div className="mt-1 flex justify-center items-center cursor-pointer shadow-lg shadow-grey-800">
+        <img
+          src="https://www.omfoods.com/cdn/shop/files/OMFOODS_ORGANIC_INGREDIENTS.jpg?v=1681412891"
+          alt="Healthy Food Banner"
+          className=""
+        />
+      </div>
+      <div className="w-fit mx-auto grid grid-cols-1 lg:grid-cols-3 md:grid-cols-2 sm:grid-cols-2 justify-items-center justify-center gap-y-20 gap-x-14 mt-10 mb-5">
+        {orgData.map((ele, i) => (
+          <OrgPro
+            key={i}
+            title={ele.title}
+            image={ele.image}
+            price={ele.price_inr}
+          />
+        ))}
       </div>
     </div>
   );
