6ef87ead67224076a40835f7d79fe8e6ea303c16
mdfaizan973 <faizan.md9735@gmail.com>
2023-10-15 11:40:50 +0530

    able to make login UI

--- organics/src/Components/AllRoutes.jsx
+++ organics/src/Components/AllRoutes.jsx
@@ -2,12 +2,14 @@
 import { Routes, Route } from "react-router-dom";
 import Home from "../Pages/Landing/Home";
 import Blog from "../Pages/Blog";
+import LoginPage from "../Pages/LoginPage";
 export default function AllRoutes() {
   return (
     <div>
       <Routes>
         <Route path="/" element={<Home />}></Route>
         <Route path="/blogs" element={<Blog />}></Route>
+        <Route path="/login" element={<LoginPage />}></Route>
       </Routes>
     </div>
   );
--- organics/src/Components/Navbar.jsx
+++ organics/src/Components/Navbar.jsx
@@ -1,21 +1,25 @@
 // import React from 'react'
 import { AiOutlineShopping, AiOutlineUser } from "react-icons/ai";
+import { Link as RouterLink } from "react-router-dom";
 export default function Navbar() {
   return (
     <div>
       <nav className="sticky inset-0 z-10 block h-max w-full max-w-full rounded-none border border-white/80 bg-white bg-opacity-80 py-2 px-4 text-white shadow-md backdrop-blur-2xl backdrop-saturate-200 lg:px-8 lg:py-4">
         <div>
           <div className="container mx-auto flex items-center justify-between text-gray-900">
-            <a
-              href="#"
-              className="mr-4 block cursor-pointer py-1.5 font-sans text-xl font-bold leading-normal text-inherit antialiased font-semibold"
-            >
-              <span className="text-green-600">Organic Store</span>
-              {/* <img
+            <RouterLink to="/">
+              <a
+                href="#"
+                className="mr-4 block cursor-pointer py-1.5 font-sans text-xl font-bold leading-normal text-inherit antialiased font-semibold"
+              >
+                <span className="text-green-600">Organic Store</span>
+                {/* <img
                 className=""
                 src="https://user-images.githubusercontent.com/106812942/275217471-417cd4b2-e456-4413-b8d0-4036e7537692.png"
               /> */}
-            </a>
+              </a>
+            </RouterLink>
+
             <ul className="hidden items-center gap-6 lg:flex">
               <li className="block p-1 font-sans text-bs font-normal leading-normal text-inherit antialiased font-semibold ">
                 <a className="flex items-center" href="#">
@@ -61,13 +65,15 @@ export default function Navbar() {
               >
                 <AiOutlineShopping />
               </button>
-              <button
-                className="middle none center hidden rounded-lg bg-gradient-to-tr from-green-600 to-green-400 py-2 px-4 font-sans text-xs font-bold uppercase text-white shadow-md shadow-green-500/20 transition-all hover:shadow-lg hover:shadow-green-500/40 active:opacity-[0.85] disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none lg:inline-block"
-                type="button"
-                data-ripple-light="true"
-              >
-                <AiOutlineUser />
-              </button>
+              <RouterLink to="/login">
+                <button
+                  className="middle none center hidden rounded-lg bg-gradient-to-tr from-green-600 to-green-400 py-2 px-4 font-sans text-xs font-bold uppercase text-white shadow-md shadow-green-500/20 transition-all hover:shadow-lg hover:shadow-green-500/40 active:opacity-[0.85] disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none lg:inline-block"
+                  type="button"
+                  data-ripple-light="true"
+                >
+                  <AiOutlineUser />
+                </button>
+              </RouterLink>
             </div>
 
             <button
