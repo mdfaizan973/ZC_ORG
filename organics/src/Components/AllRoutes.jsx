8f144dc1a7edf1fda80e6956e72d04f88fca2358
mdfaizan973 <faizan.md9735@gmail.com>
2023-10-15 11:46:42 +0530

    able to make Signup UI

--- organics/src/Components/AllRoutes.jsx
+++ organics/src/Components/AllRoutes.jsx
@@ -3,6 +3,7 @@ import { Routes, Route } from "react-router-dom";
 import Home from "../Pages/Landing/Home";
 import Blog from "../Pages/Blog";
 import LoginPage from "../Pages/LoginPage";
+import SignupPage from "../Pages/SignupPage";
 export default function AllRoutes() {
   return (
     <div>
@@ -10,6 +11,7 @@ export default function AllRoutes() {
         <Route path="/" element={<Home />}></Route>
         <Route path="/blogs" element={<Blog />}></Route>
         <Route path="/login" element={<LoginPage />}></Route>
+        <Route path="/register" element={<SignupPage />}></Route>
       </Routes>
     </div>
   );
--- organics/src/Pages/SignupPage.jsx
+++ organics/src/Pages/SignupPage.jsx
@@ -1,17 +1,22 @@
 // import React from 'react'
-
 import { Link as RouterLink } from "react-router-dom";
 
-export default function LoginPage() {
+export default function SignupPage() {
   return (
     <div className=" w-full h-screen flex items-center justify-center">
       <div className="relative flex w-96 flex-col rounded-xl bg-white bg-clip-border text-gray-700 shadow-md">
         <div className="relative mx-4 -mt-6 mb-4 grid h-28 place-items-center overflow-hidden rounded-xl bg-gradient-to-tr from-green-600 to-green-400 bg-clip-border text-white shadow-lg shadow-green-500/40">
           <h3 className="block font-sans text-3xl font-semibold leading-snug tracking-normal text-white antialiased">
-            Sign In
+            Create Account
           </h3>
         </div>
         <div className="flex flex-col gap-4 p-6">
+          <div className="relative h-11 w-full min-w-[200px]">
+            <input className="peer h-full w-full rounded-md border border-blue-gray-200 border-t-transparent bg-transparent px-3 py-3 font-sans text-sm font-normal text-blue-gray-700 outline outline-0 transition-all placeholder-shown:border placeholder-shown:border-blue-gray-200 placeholder-shown:border-t-blue-gray-200 focus:border-2 focus:border-green-500 focus:border-t-transparent focus:outline-0 disabled:border-0 disabled:bg-blue-gray-50" />
+            <label className="before:content[' '] after:content[' '] pointer-events-none absolute left-0 -top-1.5 flex h-full w-full select-none text-[11px] font-normal leading-tight text-blue-gray-400 transition-all before:pointer-events-none before:mt-[6.5px] before:mr-1 before:box-border before:block before:h-1.5 before:w-2.5 before:rounded-tl-md before:border-t before:border-l before:border-blue-gray-200 before:transition-all after:pointer-events-none after:mt-[6.5px] after:ml-1 after:box-border after:block after:h-1.5 after:w-2.5 after:flex-grow after:rounded-tr-md after:border-t after:border-r after:border-blue-gray-200 after:transition-all peer-placeholder-shown:text-sm peer-placeholder-shown:leading-[4.1] peer-placeholder-shown:text-blue-gray-500 peer-placeholder-shown:before:border-transparent peer-placeholder-shown:after:border-transparent peer-focus:text-[11px] peer-focus:leading-tight peer-focus:text-green-500 peer-focus:before:border-t-2 peer-focus:before:border-l-2 peer-focus:before:!border-green-500 peer-focus:after:border-t-2 peer-focus:after:border-r-2 peer-focus:after:!border-green-500 peer-disabled:text-transparent peer-disabled:before:border-transparent peer-disabled:after:border-transparent peer-disabled:peer-placeholder-shown:text-blue-gray-500">
+              Enter Name
+            </label>
+          </div>
           <div className="relative h-11 w-full min-w-[200px]">
             <input className="peer h-full w-full rounded-md border border-blue-gray-200 border-t-transparent bg-transparent px-3 py-3 font-sans text-sm font-normal text-blue-gray-700 outline outline-0 transition-all placeholder-shown:border placeholder-shown:border-blue-gray-200 placeholder-shown:border-t-blue-gray-200 focus:border-2 focus:border-green-500 focus:border-t-transparent focus:outline-0 disabled:border-0 disabled:bg-blue-gray-50" />
             <label className="before:content[' '] after:content[' '] pointer-events-none absolute left-0 -top-1.5 flex h-full w-full select-none text-[11px] font-normal leading-tight text-blue-gray-400 transition-all before:pointer-events-none before:mt-[6.5px] before:mr-1 before:box-border before:block before:h-1.5 before:w-2.5 before:rounded-tl-md before:border-t before:border-l before:border-blue-gray-200 before:transition-all after:pointer-events-none after:mt-[6.5px] after:ml-1 after:box-border after:block after:h-1.5 after:w-2.5 after:flex-grow after:rounded-tr-md after:border-t after:border-r after:border-blue-gray-200 after:transition-all peer-placeholder-shown:text-sm peer-placeholder-shown:leading-[4.1] peer-placeholder-shown:text-blue-gray-500 peer-placeholder-shown:before:border-transparent peer-placeholder-shown:after:border-transparent peer-focus:text-[11px] peer-focus:leading-tight peer-focus:text-green-500 peer-focus:before:border-t-2 peer-focus:before:border-l-2 peer-focus:before:!border-green-500 peer-focus:after:border-t-2 peer-focus:after:border-r-2 peer-focus:after:!border-green-500 peer-disabled:text-transparent peer-disabled:before:border-transparent peer-disabled:after:border-transparent peer-disabled:peer-placeholder-shown:text-blue-gray-500">
@@ -68,16 +73,17 @@ export default function LoginPage() {
             type="button"
             data-ripple-light="true"
           >
-            Sign In
+            Sign Up
           </button>
           <p className="mt-6 flex justify-center font-sans text-sm font-light leading-normal text-inherit antialiased">
             Dont have an account?
-            <RouterLink to="/register">
+            <RouterLink to="/login">
+              {" "}
               <a
                 href="#signup"
                 className="ml-1 block font-sans text-sm font-bold leading-normal text-green-500 antialiased"
               >
-                Sign up
+                LogIn
               </a>
             </RouterLink>
           </p>
