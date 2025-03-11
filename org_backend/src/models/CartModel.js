d8a896320620703369f0da99ac34495d077fbbd1
Md Faizan <106812942+mdfaizan973@users.noreply.github.com>
2025-03-11 16:50:19 +0530

    added inherit schema to the Cart
    added inherit schema to the Cart

--- org_backend/src/models/CartModel.js
+++ org_backend/src/models/CartModel.js
@@ -1,8 +1,25 @@
+// const mongoose = require("mongoose");
+// const ProductSchemaModel = require("./ProductsModel");
+// const CartSchema = new mongoose.Schema(
+//   {
+//     ...ProductSchemaModel.obj,
+//     userId: { type: String, required: true },
+//     userName: { type: String, required: true },
+//   },
+//   { timestamps: true }
+// );
+
+// const CartSchemaModel = mongoose.model("cartProduct", CartSchema);
+// module.exports = CartSchemaModel;
+
 const mongoose = require("mongoose");
 const ProductSchemaModel = require("./ProductsModel");
+// Extract only the schema definition (not the model)
+const ProductSchemaModelDefinition = ProductSchemaModel.schema.obj;
+
 const CartSchema = new mongoose.Schema(
   {
-    ...ProductSchemaModel.obj,
+    ...ProductSchemaModelDefinition,
     userId: { type: String, required: true },
     userName: { type: String, required: true },
   },
@@ -11,3 +28,4 @@ const CartSchema = new mongoose.Schema(
 
 const CartSchemaModel = mongoose.model("cartProduct", CartSchema);
 module.exports = CartSchemaModel;
+
